46530ca823a20b953937a88ca10101378a82ac29
Fernando Muniz <fernando.hon@live.com>
2025-06-09 20:15:15 -0300

    1.4.1

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -61,7 +61,7 @@ async function fetchDashboardData(token) {
     document.getElementById('total-despesas').textContent = `R$ ${totalDespesas.toFixed(2)}`;
 
     
-    renderPizzaCharts(despesasPorCategoriaMes).reverce();
+    renderPizzaCharts(despesasPorCategoriaMes);
   } catch (error) {
     console.error('Erro ao carregar dados do dashboard:', error);
   }
